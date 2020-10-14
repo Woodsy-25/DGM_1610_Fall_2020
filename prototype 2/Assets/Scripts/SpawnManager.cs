0ee47c80e8dca17f8d676ccd11e8e6a55a7f4d0d
Tyler Woods <tywoodsaz@gmail.com>
2020-10-13 21:34:50 -0600

    updating for laptop

--- prototype 2/Assets/Scripts/SpawnManager.cs
+++ prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,11 +7,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     public float spawnRangeX = 16;
     public float spawnRangeZ = 20;
+    public float startDelay = 2;
+    public float spawnTime = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnTime);
     }
 
     // Update is called once per frame
@@ -19,10 +21,15 @@ public class SpawnManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.S))
         {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
-
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[0].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[0].transform.rotation);
     }
 }

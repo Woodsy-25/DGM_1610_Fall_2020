19cd9d3844c4efa587a47d0ed18043a0d8e79055
Tyler Woods <tywoodsaz@gmail.com>
2020-10-25 14:09:53 -0600

    created prototype 3

---
+++ Prototype 3/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private RigidBody playerRb;
+    public float jumpForce = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<RigidBody>();
+        playerRb.AddForce(Vector3 * jumpForce);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

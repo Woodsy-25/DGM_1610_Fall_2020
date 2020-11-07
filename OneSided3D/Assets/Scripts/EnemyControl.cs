bb1325ff0d0861ad983fe92b64055e3e19b3ee44
Tyler Woods <tywoodsaz@gmail.com>
2020-11-06 20:41:39 -0700

    added final project as a 3d project instead of a 2d

---
+++ OneSided3D/Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    public bool isIt = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ OneSided3D/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float horizontalInput;
+    public float verticalInput;
+
+    public bool isIt = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //takes care of the player movement
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+    }
+}

072ade706c8d1039e04f1d0d4cb0c2819cee5191
Tyler Woods <tywoodsaz@gmail.com>
2020-11-09 19:58:23 -0700

    started prototype 4

---
+++ prototype 4/Assets/Scripts/RotateCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCam : MonoBehaviour
+{
+
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horzontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, -horzontalInput * rotationSpeed * Time.deltaTime);
+    }
+}

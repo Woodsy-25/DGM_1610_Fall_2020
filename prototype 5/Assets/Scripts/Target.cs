c86fed9a6d934d1ee59b3a8565596df3f67b06ce
Tyler Woods <tywoodsaz@gmail.com>
2020-12-01 19:43:24 -0700

    added and started prototype 5

---
+++ prototype 5/Assets/Scripts/Target.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    private float minSpeed = 12, maxSpeed = 16, maxTorque = 10, xRange = 4, ySpawnPos = -6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque));
+        transform.position = new Vector3(Random.Range(-xRange, xRange), -ySpawnPos, 0);
+    }
+
+    Vector3 RandomForce()
+    {
+        retrun Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

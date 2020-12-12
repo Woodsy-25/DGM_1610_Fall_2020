80b5416f33a670bf137e03a4c5b3be2b15360309
Tyler Woods <tywoodsaz@gmail.com>
2020-12-11 18:18:01 -0700

    finished unity 5 and added assets to final

---
+++ prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    public bool isGameActive;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+    public GameObject titleScreen;
+    private int score;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //spawns in the objects
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    // updates the score based on the object that is clicked
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    //when an objects is missed it showes game over and a restate button
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+    //when the restart button is pushed it reloads the game
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    // sets the game up and applies the difficulty
+    public void StartGame(int difficulty)
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        spawnRate /= difficulty;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+        
+    }
+}
--- prototype 5/Assets/Scripts/Target.cs
+++ prototype 5/Assets/Scripts/Target.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    
     private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minSpeed = 12, maxSpeed = 16, maxSpin = 10, xRange = 4, ySpawn = -6;
+    public int pointValue;
+    public ParticleSystem explosionPartical;
     
+    // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTroque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomSpin(), RandomSpin(), RandomSpin(), ForceMode.Impulse);
+
+        transform.position = (RandomSpawn());
     }
 
     // Update is called once per frame
@@ -21,4 +27,40 @@ public class Target : MonoBehaviour
     {
         
     }
+    //finds a random force to apply
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    //finds a random spin to apply
+    float RandomSpin()
+    {
+        return Random.Range(-maxSpin, maxSpin);
+    }
+    //finds a random spawn position
+    Vector3 RandomSpawn()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawn);
+    }
+    //destroys objects when they are clicked
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionPartical, transform.position, explosionPartical.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
+    }
+    //destoys objects when they hit the sensor and if its a good object it ends the game
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
 }
